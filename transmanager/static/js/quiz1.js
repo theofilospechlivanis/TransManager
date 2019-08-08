6baddda277a8e7e34b955aa73dc1c78f9036c25a
Theofilos Pechlivanis <theofilos.pechlivanis@gmail.com>
2019-08-08 22:06:23 +0300

    Modify space indents.

--- transmanager/static/js/quiz1.js
+++ transmanager/static/js/quiz1.js
@@ -1,10 +1,10 @@
-function getInput(){
+function getInput() {
     var frm = document.getElementById("form-timetable");
     var timetable = frm.elements[0].value;
     document.getElementById("results").innerHTML = timetable;
 }
 
-function getResults(){
+function getResults() {
     getInput();
 }
 
